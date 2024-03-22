3ac3da10bbc3ecdaab4254e425d74560244a21b3
Carlosyance33 <Carlosyance34@gmail.com>
2024-03-22 16:04:00 -0400

    ultimos cambios

--- script1.js
+++ script1.js
@@ -7,7 +7,7 @@ axios.get("https://leonardoapi.onrender.com/songs")
         res.data.songs.map((song) => {
 
             const div = document.createElement("div")
-            div.classList.add("canciones")
+            div.classList.add("cancion")  /* div.classlist= es para agregar una clase     */
 
             div.innerHTML = `
             <img class="covers" src="${song.path.front}" alt="">
@@ -20,6 +20,20 @@ axios.get("https://leonardoapi.onrender.com/songs")
             
         
             `
+            div.addEventListener ("click", () => {
+               document.getElementById ("current-song-img").setAttribute ('src' , song.path.front)
+               document.getElementById ( "audios").setAttribute ('src', song.path.audio)
+               document.getElementById ("titulo__musica") .innerHTML = song.title
+               document.getElementById ("nombre__artista") .innerHTML = song.author
+               document.getElementById ("tiempo") .innerHTML = song.duration
+
+            })
             tracklist.appendChild(div)
         })
     })
+
+
+
+    /* setattribute= funciona para agregar un atributo     */
+    /* innerhtml sirve para agregar contenido de texto entre la etiqueta
+    /*  div.innerhtml agregar el contenido en el div */
